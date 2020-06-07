9c98664046d8fb93da5e8f7a098683592be55b0e
ParamagicDev <konnor5456@gmail.com>
2020-06-07 09:07:19 -0400

    add assertions

--- templates/cypress/integration/app_test.js
+++ templates/cypress/integration/app_test.js
@@ -5,7 +5,12 @@ describe("Testing that links exist in the navbar", () => {
 
   context("navbar links appear on all pages", () => {
     cy.get('[href="/"]').click();
+    assert.equal(cy.url(), "/");
+
     cy.get('[href="/posts"]').click();
+    assert.equal(cy.url(), "/posts");
+
     cy.get('[href="/about"]').click();
+    assert.equal(cy.url(), "/about");
   });
 });
